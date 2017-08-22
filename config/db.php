03020e6d952a00641e8fb9ecba806ecebb77420e
Vinicius Assumpção <vinicius.assumpcao@yahoo.com.br>
2017-08-21 21:39:11 -0300

    Finanças
    Carga Inicial do projeto.

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'development' => [
+		'driver' => 'mysql',
+		'host' => 'localhost',
+		'database' => 'financas',
+		'username' => 'root',
+		'password' => 'root',
+		'charset' => 'utf8',
+		'collation' => 'utf8_unicode_ci'
+	]
+];
